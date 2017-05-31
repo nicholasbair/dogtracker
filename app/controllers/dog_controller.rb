31b7cf3eddcd3266d52498a030decaafed1d71df
Nick Bair <nickbair@golfingbuddha.com>
2017-05-31 15:42:19 -0400

    WIP #7.

--- app/controllers/dog_controller.rb
+++ app/controllers/dog_controller.rb
@@ -6,24 +6,31 @@ class DogController < ApplicationController
   end
 
   get '/dogs' do
+    @dogs = Dog.find_by(user_id: current_user.id)
+    erb :'dogs/index'
+  end
+
+  get '/dogs/:id' do
+    @dog = Dog.find(params[:id])
+
+    erb :'dogs/show'
   end
 
   get '/dogs/new' do
+    erb :'dogs/new'
   end
 
   post '/dogs' do
+    current_user.dogs.build(name: params[:name])
 
+    redirect '/dogs'
   end
 
   get '/dogs/:id/edit' do
-
+    erb :'dogs/edit'
   end
 
   patch '/dogs/:id' do
-
-  end
-
-  get 'dogs/:id/delete' do
 
   end
 
