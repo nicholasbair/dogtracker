a110ec2de7068bf60af353fe7cd915c16e1d5eeb
Nick Bair <nickbair@golfingbuddha.com>
2017-05-31 14:49:55 -0400

    Add dotenv for session secret, closes #12.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 require './config/environment'
+require 'securerandom'
 
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "safety first"
+    set :session_secret, ENV["SESSION_SECRET"] || SecureRandom.hex(64)
   end
 
   get '/' do
