d4cd7c4129b291734912950b7adde3ee1b9d26d9
Nick Bair <nickbair@golfingbuddha.com>
2017-05-31 16:07:35 -0400

    WIP #8.

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -6,6 +6,32 @@ class ActivityController < ApplicationController
   end
 
   get '/activities' do
+    @activities = Activity.all
     erb :'/activities/index'
+  end
+
+  get '/activities/new' do
+    erb :'/activities/new'
+  end
+
+  get '/activities/:id' do
+    @activity = Activity.find(params[:id])
+    erb :'activities/show'
+  end
+
+  post '/activities' do
+
+  end
+
+  get '/activities/:id/edit' do
+
+  end
+
+  patch '/activities/:id' do
+
+  end
+
+  delete 'activities/:id/delete' do
+
   end
 end
