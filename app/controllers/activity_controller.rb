45613fd1db0a2981f19e5c19b401ef2ab97c472f
Nick Bair <nickbair@golfingbuddha.com>
2017-06-01 13:26:33 -0400

    Build basic crud routes for activity, closes #8.

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -21,20 +21,29 @@ class ActivityController < ApplicationController
   end
 
   post '/activities' do
+    params[:dogs].each do |dog_id|
+      Dog.find(dog_id).activities.build(name: params[:name], duration: params[:duration]).save
+    end
     redirect '/activities'
   end
 
   get '/activities/:id/edit' do
     @activity = Activity.find(params[:id])
+    @dogs = current_user.dogs
+    erb :'activities/edit'
   end
 
   patch '/activities/:id' do
     activity = Activity.find(params[:id])
-    activity.update()
+    activity.update(
+      name: params[:name],
+      duration: params[:duration],
+      dog_id: params[:dogs][0]
+    )
     redirect '/activities'
   end
 
-  delete 'activities/:id/delete' do
+  delete '/activities/:id/delete' do
     activity = Activity.find(params[:id])
     Activity.destroy(activity.id)
     redirect '/activities'
