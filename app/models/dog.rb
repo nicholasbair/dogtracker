e1d38edc8a5c5153cbd51732fa1f49a105d78413
Nick Bair <nickbair@golfingbuddha.com>
2017-06-02 09:48:09 -0400

    Scope Dog name uniqueness to user, closes #21.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,5 +2,9 @@ class Dog < ActiveRecord::Base
   belongs_to :user
   has_many :activity_dogs
   has_many :activities, through: :activity_dogs
-  validates :name, :user_id, presence: true
+  validates :user_id, presence: true
+  validates :name, uniqueness: {
+    scope: :user,
+    message: "should only have one dog with that name"
+  }
 end
